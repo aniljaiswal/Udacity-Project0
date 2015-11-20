afadfe76aec0cd072e7391181738e9591f3381c5
Anil Jaiswal <er.anil.jaiswal@gmail.com>
2015-11-20 10:25:00 +0530

    Completed Udacity-Project0, tested on 4.0.3 and above.

--- src/main/java/com/aniljaiswal/udacity_project0/MainActivity.java
+++ src/main/java/com/aniljaiswal/udacity_project0/MainActivity.java
@@ -1,13 +1,11 @@
 package com.aniljaiswal.udacity_project0;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,36 +15,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    /**
+     * Handle button clicks dynamically and show a toast message
+     * @param button the button being clicked
+     */
+    public void buttonOnClick(View button) {
+
+        Button b = (Button)button;
+
+        Toast.makeText(MainActivity.this, "This button will launch my " + b.getText().toString() + " app.", Toast.LENGTH_SHORT).show();
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
